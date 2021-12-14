213463e5d3d8c440339b46d60ae9d54c2654caf5
unknown <irinagithub7>
2021-12-14 23:21:58 +0500

    15App Frag Intent

---
+++ app/src/main/java/com/example/my15app/AboutFragment.kt
@@ -0,0 +1,40 @@
+package com.example.my15app
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class AboutFragment : Fragment() {
+    lateinit var developerRecyclerView: RecyclerView
+    lateinit var showWebSiteButton: Button
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val developersNames: List<String> = listOf("Maxim", "Ivan")
+
+        showWebSiteButton = view.findViewById(R.id.show_web_site_button)
+
+        developerRecyclerView = view.findViewById(R.id.developer_recycler_view)
+        developerRecyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        developerRecyclerView.adapter = DeveloperAdapter(developersNames)
+
+        showWebSiteButton.setOnClickListener {
+            val link = Uri.parse("https://google.com/")
+            val intent = Intent(Intent.ACTION_VIEW, link)
+            context?.startActivity(intent)
+        }
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/my15app/DeveloperViewHolder.kt
@@ -0,0 +1,14 @@
+package com.example.my15app
+
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+class DeveloperViewHolder(itemView: View): RecyclerView.ViewHolder (itemView) {
+    private val developerNameTextView: TextView =
+        itemView.findViewById(R.id.developer_name_text_view)
+    fun bind(name : String){
+        developerNameTextView.text = name
+
+    }
+}
